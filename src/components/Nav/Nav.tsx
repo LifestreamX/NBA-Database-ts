952afacc879abd86c719f5afa2ead5bbad328dd2
haloandfable <tylerallen@live.com>
2022-10-17 14:52:23 -0400

    first commit

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className='nav-logo'>LOGO</div>
+      <div className='nav-title'>NBA DATABASE</div>
+    </nav>
+  );
+};
+
+export default Nav;
