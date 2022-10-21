dc19ab1067b8729eb278091a4e2eb8a2757664a4
haloandfable <tylerallen@live.com>
2022-10-21 12:48:28 -0400

    set up card styles and api endpoints

--- src/components/cards/card/Card.tsx
+++ src/components/cards/card/Card.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { getPlayers } from '../../API';
 import { useQuery } from 'react-query';
 import './Card.scss';
 
+export type PlayerType = {
+  first_name: string;
+  height_feet: number;
+  height_inches: number;
+  id: number;
+  last_name: string;
+  position: string;
+  weight_pounds: number;
+  team: any;
+};
+
 const Card = () => {
+  const [players, setPlayers] = useState([] as PlayerType[]);
   const { data, isLoading, error } = useQuery<any>(['players'], getPlayers);
 
-  // console.log(data.data);
+  useEffect(() => {
+    setPlayers(data?.data);
+  });
+
+  console.log(data?.data);
   return (
-    <main>
-        <div className='card'>
+    <div className='card-wrapper'>
+      {players?.map((player) => (
+        <div className='card' key={player.id}>
           <div className='content'>
-            <section className='front'>Front</section>
-            <section className='back'>Bfdsfsdf</section>
+            <div className='font'>
+              <p>First Name: {player.first_name}</p>
+              <p>Last Name: {player.last_name}</p>
+              <p>Position: {player.position}</p>
+            </div>
+            <div className='back'>
+              <p>
+                Team: {player.team.city} {player.team.name}
+              </p>
+              <p>Conference: {player.team.conference}</p>
+            </div>
           </div>
         </div>
-    </main>
+      ))}
+    </div>
   );
 };
 
