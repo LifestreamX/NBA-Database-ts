dcaf9b92b000aff3b296e070da537b3e9784a6b1
haloandfable <tylerallen@live.com>
2022-12-22 14:11:28 -0500

    finsihed home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -11,9 +11,7 @@ const Home = () => {
         <source src={Background} />
       </video>
       <div className='home-button-wrapper'>
-        <Link to='/database'>
-          <HomeButton />
-        </Link>
+        <HomeButton />
       </div>
     </main>
   );
--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './HomeButton.scss';
 
 const HomeButton = () => {
-  return <button className='enter-button'>ENTER DATABASE</button>;
+  return (
+    <Link to='/database'>
+      <div className='btn-2'>
+        <div>
+          <span >ENTER DATABASE</span>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default HomeButton;
